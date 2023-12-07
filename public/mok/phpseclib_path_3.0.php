8729d73c7d1d42c928ae9df4f818a3efcdfcfd5b
mateo <kyongdok0906@outlook.com>
2023-12-08 01:12:57 +0800

    upset a bit

--- public/mok/phpseclib_path_3.0.php
+++ public/mok/phpseclib_path_3.0.php
@@ -6,7 +6,6 @@
         die('1000|phpseclib 3.0.x의 autoload.php 경로가 일치하지 않습니다. 확인해 주세요.');
     } else {
         require_once $phpseclib_autoload;
-        //use  \phpseclib3\Crypt\PublicKeyLoader; // basic problem
         // fixed with command :: $sudo composer require phpseclib/phpseclib:3.0.x-dev -W
     }
 ?>
